255c8c7c8378fdc0ee0a2844e823e11c86561615
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-02 14:09:34 +0300

    Refactor axios.

--- src/github.js
+++ src/github.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
+
 require('dotenv').config();
 
-const instance = axios.create({
-  baseURL: 'https://api.github.com',
-  timeout: 1000,
-  headers: {
-    Accept: 'application/vnd.github.v3+json',
-    Authorization: `token ${process.env.GITHUB_TOKEN}`,
-  },
-});
+let axiosI;
+
+const axiosInstance = () => {
+  if (axiosI) {
+    return axiosI;
+  }
+
+  if (!process.env.GITHUB_TOKEN) {
+    throw Error('No GitHub token!');
+  }
+
+  axiosI = axios.create({
+    baseURL: 'https://api.github.com',
+    timeout: 1000,
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  return axiosI;
+};
+
 
 const fetchGitHubRepos = async () => {
+  const instance = axiosInstance();
   const axiosResult = await instance.get('/user/repos');
   const { data } = axiosResult;
   console.log(data);
