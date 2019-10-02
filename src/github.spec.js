7ab331a2f1973b23afaa0e9c7f368b8d3f436109
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-02 14:04:37 +0300

    Add github.

--- src/github.spec.js
+++ src/github.spec.js
@@ -1,7 +1,17 @@
+import unmock, { transform } from 'unmock';
 import fetchGitHubRepos from './github';
 
+let github;
+
 describe('Fetching GitHub repositories', () => {
-  it('fetches repositories', () => {
-    expect(1).toBe(1);
+  beforeAll(() => { github = unmock.on().services.githubv3; });
+  afterAll(() => { unmock.off(); });
+
+  beforeEach(() => {
+    github.state(transform.withCodes(200));
+  });
+  it('fetches repositories', async () => {
+    const repos = await fetchGitHubRepos();
+    expect(repos.length).toBeGreaterThan(0);
   });
 });
