19c7204489cc7d3844d08e8dcb873074d9664cf3
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-06 10:54:03 +0300

    Add test command, hope it doesn't break. (#2)
    * Add test command, add breaking test.
    
    * Fix bug.

--- src/github.spec.js
+++ src/github.spec.js
@@ -1,10 +1,13 @@
-/* global describe, beforeAll, afterAll, beforeEach, it, expect */  // eslint-disable-line
+/* global describe, beforeAll, afterAll, beforeEach, it, expect */ // eslint-disable-line
 // import fetchGitHubRepos from './github';
 
-process.env.GITHUB_TOKEN = 'fake';
+process.env.GITHUB_TOKEN = "fake";
 
-describe.skip('Fetching GitHub repositories', () => {
-  it.todo('should return an array of repositories');
-  it.todo('should transform repositories as expected');
-  it.todo('should not return private repositories');
+describe("Fetching GitHub repositories", () => {
+  it("should fail", () => {
+    expect(1).toBe(1);
+  });
+  it.todo("should return an array of repositories");
+  it.todo("should transform repositories as expected");
+  it.todo("should not return private repositories");
 });
